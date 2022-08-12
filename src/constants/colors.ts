2a5b81611c47b430ff91e767f6e779f89f2fb7f3
Luis Mera <luismera86@gmail.com>
2022-08-12 20:48:55 -0300

    bases del proyecto iniciadas

---
+++ src/constants/colors.ts
@@ -0,0 +1,7 @@
+export default {
+    primary: '#2B2D42',
+    secondary: '#8D99AE',
+    background: '#EDF2F4',
+    tertiary: '#EDF2F4',
+    fourth: '#EF233C'
+}
---
+++ src/styles.ts
@@ -0,0 +1,6 @@
+import { StyleSheet } from 'react-native'
+import colors from './constants/colors'
+
+export const style = StyleSheet.create({
+	container: {},
+})
