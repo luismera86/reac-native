5228e62e86c8f64b9e2db287f8d2e07da8116bcb
Luis Mera <luismera86@gmail.com>
2022-08-22 10:23:05 -0300

    Comienzo del desafío 5, sin reacomodar los archivos del desafio 4

---
+++ src/navigator/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native'
+
+import React from 'react'
+
+type Props = {}
+
+const RootNavigator = (props: Props) => {
+  return (
+    <View>
+      <Text>RootNavigator</Text>
+    </View>
+  )
+}
+
+export default RootNavigator
